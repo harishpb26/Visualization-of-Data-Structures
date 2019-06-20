a10a4cdcec0aaad5bd7fac5a5fefd5946a879c91
harishpb26 <40353558+harishpb26@users.noreply.github.com>
2019-06-20 21:04:47 +0530

    Update cgrammar.py

--- cgrammar.py
+++ cgrammar.py
@@ -103,7 +103,6 @@ def t_NEWLINE(t):
 	t.lexer.lineno += len(t.value)
 	
 
-
 #ignored characters such as spaces
 t_ignore  = ' \t'
 
@@ -116,6 +115,7 @@ def t_error(t):
 def t_COMMENT(t):
     r'\#.*'
     pass
+
 
 #build the lexer
 lexer = lex.lex()
@@ -147,6 +147,8 @@ def p_primary_expression(p):
 						|	pointer_declare SEMICOLON primary_expression
 						|	pointer_assign SEMICOLON primary_expression
 						|	structure_declare primary_expression
+						|	struct_pointer_declare SEMICOLON primary_expression
+						|	struct_pointer_assign SEMICOLON primary_expression
 						|	dynamic SEMICOLON primary_expression
 						|	expression SEMICOLON 
 						|	var_assign SEMICOLON
@@ -155,6 +157,8 @@ def p_primary_expression(p):
 						|	pointer_assign SEMICOLON 
 						|	pointer_declare SEMICOLON
 						|	structure_declare
+						|	struct_pointer_declare SEMICOLON
+						|	struct_pointer_assign SEMICOLON
 						|	dynamic SEMICOLON
 						
 	'''	
@@ -173,7 +177,7 @@ def p_var_declare(p):
 	var_declare	:	INT	ID ASSIGN expression
 				|	FLOAT ID ASSIGN expression 
 	'''
-	print("variable ", p[2], "of type ", p[1])
+	#print("variable ", p[2], "of type ", p[1])
 	if(p[2] not in var_type):
 		var_type[p[2]] = p[1]
 	var_value[p[2]] = p[4]
@@ -184,36 +188,26 @@ def p_var_assign(p):
 	var_assign	:	ID ASSIGN expression
 	
 	'''
-	if(p[1] not in var_type):
-		print(p[1], "not declared")
-	else:
-		var_value[p[1]] = p[3]
-	p[0] = (p[2], p[1], p[3])
+	var_value[p[1]] = p[3]
+	#p[0] = (p[2], p[1], p[3])
+
 
 def p_pointer_declare_without_assign(p):
 	'''
 	pointer_declare_without_assign : INT MULT ID
 				    | FLOAT MULT ID
 	'''
-	if(p[3] not in var_type ):
-		print("pointer ", p[3] , " of type ", p[1])
-		var_type[p[3]] = p[1]+"*"
-	else:
-		print("redefinition")
+	
+	#print("pointer ", p[3] , " of type ", p[1])
+	var_type[p[3]] = p[1] + "*"
+
 
 def p_pointer_assign(p):
 	'''
 	pointer_assign : ID ASSIGN ADDR ID
 				   
 	'''
-	if(p[1]==p[4]):
-		print("Conflicting types")
-	elif(p[4] not in var_type):
-		print(p[4], "not declared")
-	elif(p[1] not in var_type ):
-		print(p[1], "not declared")
-	else:
-		var_value[p[1]] = "&"+p[4]
+	var_value[p[1]] = "&" + p[4]
 
 
 def p_pointer_declare(p):
@@ -222,34 +216,82 @@ def p_pointer_declare(p):
 				   | FLOAT MULT ID ASSIGN ADDR ID
 	'''
 	
-	if(p[3]==p[6]):
-		print("Conflicting types")
-	elif(p[6] not in var_type):
-		print(p[6], "not declared")
-	elif(p[3] not in var_type ):
-		print("pointer ", p[3] , " of type ", p[1])
-		var_type[p[3]] = p[1]+"*"
-		var_value[p[3]] = "&"+p[6]
-	else:
-		print("redefinition")
-
+	#print("pointer ", p[3] , " of type ", p[1])
+	var_type[p[3]] = p[1] + "*"
+	var_value[p[3]] = "&" + p[6]
 	
+	
+def p_struct_pointer_declare(p):
+	'''
+	struct_pointer_declare	:	STRUCT ID MULT ID 
+	'''
+	var_type[p[4]] = p[2] + "*"
+	
+
+def p_struct_pointer_assign(p):
+	'''
+	struct_pointer_assign	:	STRUCT ID MULT ID ASSIGN ID
+							| 	struct_pointer_assign
+						   
+				   
+	'''
+	var_type[p[4]] = p[2] + "*"
+	var_value[p[4]] = "&" + p[6]
+
+
+"""
+def p_struct_pointer_assign_dynamic(p):
+	'''
+	struct_pointer_assign	:	STRUCT ID MULT ID ASSIGN MALLOC LPAREN size2 RPAREN 
+	'''
+	var_type[p[4]] = p[2] + "*"
+"""
+
+
 def p_dynamic(p):
 	'''
-	dynamic	:	MALLOC LPAREN size RPAREN
-	
+	dynamic	:	INT MULT ID ASSIGN MALLOC LPAREN size1 RPAREN
+			|	FLOAT MULT ID ASSIGN MALLOC LPAREN size1 RPAREN
+			|	INT MULT ID ASSIGN MALLOC LPAREN size2 RPAREN
+			|	FLOAT MULT ID ASSIGN MALLOC LPAREN size2 RPAREN
+			|	INT MULT ID ASSIGN MALLOC LPAREN size3 RPAREN
+			|	FLOAT MULT ID ASSIGN MALLOC LPAREN size3 RPAREN
+
 	'''
-	print("dynamically allocated with", p[1], "where number of size allocated is", p[3])
-	
-	
-def p_size(p):
+	var_type[p[3]] = p[1] + "*"
+	#print("dynamically allocated ",p[3]," with malloc of size", p[7])
+
+
+def p_size1(p):
 	'''
-	size	:	SIZEOF LPAREN INT RPAREN multiplier
+	size1	:	SIZEOF LPAREN INT RPAREN multiplier
 			|	SIZEOF LPAREN FLOAT RPAREN multiplier
-			|	SIZEOF LPAREN STRUCT ID RPAREN multiplier
-			|	NUMBER	multiplier
 	'''
-	p[0] = p[3]
+	p[0] = p[3] + str(p[5])
+
+
+def p_size2(p):
+	'''
+	size2	:	SIZEOF LPAREN STRUCT ID RPAREN multiplier
+	'''
+	p[0] = p[3] + " " + p[4] + str(p[6])
+
+
+def p_size3(p):
+	'''
+	size3	:	NUMBER	multiplier
+	'''
+	p[0] = p[1] + str(p[2])
+	
+	
+def p_dynamic_init(p):
+	'''
+	dynamic :	ID ASSIGN MALLOC LPAREN size1 RPAREN
+				 |  ID ASSIGN MALLOC LPAREN size2 RPAREN
+				 |  ID ASSIGN MALLOC LPAREN size3 RPAREN
+	'''
+	#print("dynamically allocated ",p[1]," with malloc of size", p[5])
+		
 
 def p_multiplier(p):
 	'''
@@ -261,11 +303,6 @@ def p_multiplier(p):
 	'''
 	
 	
-def p_empty(p):
-	'''	
-	empty	:	
-	'''
-	
 def p_structure_declare(p):
 	'''
 	structure_declare	:	STRUCT ID LEFTBRACE content RIGHTBRACE SEMICOLON
@@ -273,20 +310,37 @@ def p_structure_declare(p):
 	'''
 	if(p[2] not in var_type):
 		var_type[p[2]] = p[1]
-	print("This is a structure",p[2])
+	#print("This is a structure",p[2])
 	
 	
 def p_content(p):
 	'''
-	content	:	INT ID SEMICOLON content
+	content	:	struct_content
+			|	INT ID SEMICOLON content
 			|	FLOAT ID SEMICOLON content
 			|	FLOAT ID SEMICOLON
 			|	INT ID SEMICOLON
 			
 	
 	'''
-	if(p[2] not in var_type):
-		var_type[p[2]] = p[1]
+	try:
+		if(p[2] not in var_type):
+			var_type[p[2]] = p[1]
+	except:
+		pass
+		
+
+def p_struct_content(p):
+	'''
+	struct_content	:	STRUCT ID MULT ID SEMICOLON struct_content content
+			 		|	STRUCT ID MULT ID SEMICOLON content
+			 		|	STRUCT ID MULT ID SEMICOLON
+	'''
+	try:
+		if(p[4] not in var_type):
+			var_type[p[4]] = p[1] + " " + p[2]
+	except:
+		pass
 
 
 def p_expression(p):
@@ -316,6 +370,11 @@ def p_expression_int_float(p):
 	p[0] = p[1]
 
 
+def p_empty(p):
+	'''	
+	empty	:	
+	'''
+
 def p_error(p):
 	print("parseError")
 
@@ -336,4 +395,3 @@ parser.parse(string)
 
 print(var_type)
 print(var_value)
-
